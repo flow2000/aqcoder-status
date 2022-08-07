885d31cd134b580ca42e0ca134f125f6369ba31a
Sort-8 <1982989137@qq.com>
2022-08-07 10:58:49 +0800

    提交站点状态

---
+++ public/config.js
@@ -0,0 +1,35 @@
+window.Config = {
+
+    // 显示标题
+    SiteName: '我的站点状态',
+
+    // UptimeRobot Api Keys
+    // 支持 Monitor-Specific 和 Read-Only
+    ApiKeys: [
+        'm792364778-a84f1a11c140849c053ab2f5',
+        'm792398334-f30d543b49ffb5f431a2cdc1',
+        'm792415886-40f4b72b4a0a952f0233bfc0',
+        'm792416006-3bc6a23642082e37d5cabc4f',
+    ],
+
+    // 日志天数
+    CountDays: 90,
+
+    // 是否显示检测站点的链接
+    ShowLink: true,
+
+    // 导航栏菜单
+    Navi: [{
+            text: 'Homepage',
+            url: 'https://panghai.top/'
+        },
+        {
+            text: 'Gitee',
+            url: 'https://gitee.com/pgh1038/pgh1038'
+        },
+        {
+            text: 'Blog',
+            url: 'https://blog.panghai.top/'
+        },
+    ],
+};
